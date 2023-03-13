638d8a153bf5ff8ebd900b9a40a9011f5206f7f3
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-03-13 16:07:17 -0500

    [Update] Expo heading 47.0v

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -11,7 +11,8 @@ export default function CategoriesScreen() {
     <FlatList  
       data={CATEGORIES}
       keyExtractor={(item)=> item.id}
-      renderItem={renderCategoryItem} 
+      renderItem={renderCategoryItem}
+      numColumns={2}
     />
   )
 }
