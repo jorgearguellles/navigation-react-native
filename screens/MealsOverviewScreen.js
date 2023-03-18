9b349b37faceb08e5c1f27d4e831355148fc4b89
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-03-17 22:28:00 -0500

    [Add] MealItem.js componenet and render meal info per meal

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 export default function MealsOverviewScreen() {
   const route = useRoute();
   const categoryId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
